f23664f1c30a5ebc26a29d0b6a7b6f69eef1aeba
Nguyen Phuc Son Tung <ngphsotu@gmail.com>
2022-11-09 17:00:45 +0700

    ...

--- lib/screens/implicit_animations/animation_foo/animated_icon.dart
+++ lib/screens/implicit_animations/animation_foo/animated_icon.dart
@@ -41,33 +41,34 @@ class _DemoAnimatedIconState extends State<DemoAnimatedIcon>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(color: Colors.black),
-        title: const Text(
-          _title,
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            if (_selected == false) {
-              _controller.forward();
-              _selected = true;
-            } else {
-              _controller.reverse();
-              _selected = false;
-            }
-          },
-          child: AnimatedIcon(
-            icon: AnimatedIcons.play_pause,
-            progress: _controller,
-            size: 150,
-            color: Colors.green,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            _title,
+            style: TextStyle(color: Colors.pink, fontSize: 20),
           ),
-        ),
+          const SizedBox(height: 20),
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                if (_selected == false) {
+                  _controller.forward();
+                  _selected = true;
+                } else {
+                  _controller.reverse();
+                  _selected = false;
+                }
+              },
+              child: AnimatedIcon(
+                icon: AnimatedIcons.play_pause,
+                progress: _controller,
+                size: 150,
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
